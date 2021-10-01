8f8cc6d8267743edc0f87b4d437039f5af2943da
Bara Konateh <bkonateh1@gmail.com>
2021-10-01 14:57:46 +0100

    Update HSection3.vue

--- src/components/howitworks/HSection3.vue
+++ src/components/howitworks/HSection3.vue
@@ -1,8 +1,43 @@
 <template>
   <div id="frame">
     <div class = "hS3_container">
-      <vueper-slides fixed-height="80vh">
-        <vueper-slide v-for="i in 3" :key="i" :title="i.toString()" />
+      <vueper-slides fixed-height="true">
+        <vueper-slide
+          v-for="(slide) in slide"
+          :key="slide.id"
+          :title="slide.title">
+          <template #content>
+            <div v-if="slide.id==1" class = "container">
+              <h1>How to mint an item?</h1>
+              <h2> Step <br><span>0{{slide.id}} </span> </h2>
+            </div>
+            <div v-if="slide.id==2" class = "container">
+              <h1>How to mint an item?</h1>
+              <h2> Step <br><span>0{{slide.id}} </span> </h2>
+            </div>
+            <div v-if="slide.id==3" class = "container">
+              <h1>How to mint an item?</h1>
+              <h2> Step <br><span>0{{slide.id}} </span> </h2>
+              <div class = "textContainer">
+                <b-row align-h="center" style="min-height: 30vh">
+                  <b-col class = "mt-5" align-self="center" >
+                    <p>Quick explanation on what and why mint an item :D<br><br>
+                      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris <br><br>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                      commodo consequat. Duis aut <br><br>
+                    </p>
+                  </b-col>
+                  <b-col align-self="center">
+                  <img src="https://res.cloudinary.com/risidio/image/upload/v1633002019/RisidioMarketplace/digitalcurrency_tqf1es.svg" alt="digitalcurrency" class="digitalcurrency">
+                  </b-col>
+                </b-row>
+              </div>
+            </div>
+          </template>
+        </vueper-slide>
       </vueper-slides>
     </div>
   </div>
@@ -19,10 +54,15 @@ export default {
   },
   props: ['content'],
   data: () => ({
-    slides: [
+    slide: [
       {
-        title: 'Slide #1',
-        content: 'Slide content.'
+        id: '1'
+      },
+      {
+        id: '2'
+      },
+      {
+        id: '3'
       }
     ]
   }),
@@ -33,7 +73,38 @@ export default {
 
 <style scoped>
 .vueperslide {
-  background-color: #170A6D;
+  background-color:#170A6D;
 }
-.vueperslides--fixed-height { height: 500px; }
+.vueperslides--fixed-height {
+  height: 70vh;
+}
+h1{
+  color:white;
+  font-family: inherit;
+  font-size: clamp(50px, 3vw, 6vw);
+  font-weight: 200;
+  padding-top: 20px;
+  text-align: center;
+}
+h2{
+  padding-top: 30px;
+  text-align: center;
+  color: white;
+  font-weight: 400;
+}
+span{
+  font-weight: 600;
+  font-size: 1.2em;
+}
+img{
+  height: 250px;
+  width: 250px;
+  margin-left: 125px;
+}
+.textContainer > *{
+  margin:auto;
+  padding-top: 100px;
+  color:white;
+  font-weight: 300;
+}
 </style>
