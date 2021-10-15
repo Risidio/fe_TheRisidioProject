49fd727d0dc21ea967aed6fc33186d3cc6066a28
Bara Konateh <bkonateh1@gmail.com>
2021-10-15 13:52:29 +0100

    bugged but works

--- src/components/three/threeTest.vue
+++ src/components/three/threeTest.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <input ref="inputField" type="file" id="input" multiple v-on:change="loadFile()">
+        <input ref="inputField" type="file" id="input" multiple>
         <img id="output"/>
         <button v-on:click="three()"> Clickidy click click</button>
         <div class = "threeD">
@@ -20,19 +20,30 @@ export default {
     }
   },
   methods: {
-    loadFile () {
-      const selectedFile = document.getElementById('input').files[0]
-      const output = document.getElementById('output')
-      output.src = URL.createObjectURL(selectedFile)
-      console.log(output.src)
-      // console.log(dddd)
-      // console.log(selectedFile)
-    },
-    three () {
+    threeObjectHandler () {
       const selectedFile = document.getElementById('input').files[0]
       console.log(selectedFile)
       const url = URL.createObjectURL(selectedFile)
       console.log(url)
+
+      return {
+        url
+      }
+    },
+    threeLights () {
+      const pointLight = new Three.PointLight(0xffffff)
+      const ambientLight = new Three.AmbientLight(0xffffff)
+      pointLight.position.set(20, 20, 20)
+
+      return {
+        pointLight,
+        ambientLight
+      }
+    },
+    threeControls (camera, renderer) {
+      const controls = new OrbitControls(camera, renderer.domElement)
+    },
+    three () {
       const scene = new Three.Scene()
       const sizes = {
         width: window.innerWidth * 0.5,
@@ -40,22 +51,22 @@ export default {
       }
       const camera = new Three.PerspectiveCamera(75, sizes.width / sizes.height, 0.10, 1000)
       const loader = new GLTFLoader()
-      const material = new Three.MeshStandardMaterial({ color: 0xFF6347, wireframe: false })
+      const material = new Three.MeshStandardMaterial({ color: 0xFF6347, wireframe: true })
       const renderer = new Three.WebGLRenderer({ canvas: document.getElementsByTagName('canvas')[0] })
-      const pointLight = new Three.PointLight(0xffffff)
-      const ambientLight = new Three.AmbientLight(0xffffff)
-      const controls = new OrbitControls(camera, renderer.domElement)
+
+      const controls = this.threeControls(camera, renderer)
+      const lights = this.threeLights()
+      const url = this.threeObjectHandler()
 
       scene.background = new Three.Color(0x8fbcd4)
       renderer.setSize(sizes.width, sizes.height)
 
       // camera.position.setX(100)
       let box = new Three.Box3()
-      pointLight.position.set(20, 20, 20)
-      scene.add(pointLight, ambientLight)
-      // 'http://localhost:8086/Lemon_Sorbet.gltf'
+
+      scene.add(lights.pointLight, lights.ambientLight)
       let obj = ''
-      loader.load(url,
+      loader.load(url.url,
         function (gltf) {
           obj = gltf.scene
           obj.material = material
@@ -73,12 +84,10 @@ export default {
         }
       )
       // These need to be run AFTER the the object has rendered
-      const height = box.min.x
-      console.log('x =  ' + height)
 
-      camera.position.setY(1.4)
-      camera.position.setX(-227)
-      camera.position.setZ(882)
+      // camera.position.setY(0)
+      // camera.position.setX(0)
+      camera.position.set(5, 0, 0)
 
       window.addEventListener('resize', () => {
         // Update sizes
@@ -93,12 +102,34 @@ export default {
         renderer.setSize(sizes.width, sizes.height)
         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
       })
+
       function animate () {
-        requestAnimationFrame(animate)
         obj.rotation.y += 0.02
+      }
+      function render () {
         renderer.render(scene, camera)
       }
-      animate()
+      function start () {
+        requestAnimationFrame(start)
+        animate()
+        render()
+        console.log('start has run once')
+        // camera.position.setY(box.min.y)
+        // camera.position.setX(box.min.x)
+        // camera.position.setZ(box.min.z)
+      }
+      function fullStart () {
+        render()
+        start()
+        console.log('fullstart has run once ')
+      }
+      setTimeout(() => {
+        camera.position.setY(box.min.y)
+        camera.position.setX(box.min.x)
+        camera.position.setZ(box.min.z)
+        console.log('camera adjusted')
+      }, 8000)
+      fullStart()
     }
   }
 }
